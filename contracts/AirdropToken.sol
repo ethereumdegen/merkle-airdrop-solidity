31fb62772c87fc1591da8d925926b58ea7d2b739
andy <andy@jonezie.com>
2021-09-07 23:10:33 -0400

    adding more metadata

--- contracts/AirdropToken.sol
+++ contracts/AirdropToken.sol
@@ -1842,9 +1842,87 @@ library Strings {
 
 
 
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 contract AirdropToken is ERC721, Ownable {
-  
+    
+    
    
     bytes32 internal _merkleRoot;
                                          
@@ -1852,15 +1930,19 @@ contract AirdropToken is ERC721, Ownable {
 
     mapping(address => bool) public hasClaimed;
 
+    address public _animationToken;
 
-    constructor(string memory name, string memory symbol, bytes32 merkleRoot) ERC721(name, symbol) {
+    uint256 constant LEVEL_TWO_AMOUNT = 2100 * (10**8);
+
+    uint256 constant LEVEL_THREE_AMOUNT = 21000 * (10**8);
+
+
+    constructor(string memory name, string memory symbol, bytes32 merkleRoot, address animationToken) ERC721(name, symbol) {
         _merkleRoot = merkleRoot;
+        _animationToken = animationToken;
     }  
 
-
-    function setBaseURI(string memory baseURI) onlyOwner public {
-        _setBaseURI(baseURI);  
-    }  
+ 
 
     /**
     * @dev Mints new NFTs
@@ -1875,12 +1957,34 @@ contract AirdropToken is ERC721, Ownable {
         
         _mint(msg.sender, nextTokenId++); 
     }
+
+    function getAnimationLevel(uint256 tokenId) public view returns (uint256) {
+        address owner = ownerOf(tokenId);
+
+        if( IERC20(_animationToken).balanceOf(owner) >= LEVEL_THREE_AMOUNT ){
+            return 3;
+        }
+
+        if( IERC20(_animationToken).balanceOf(owner) >= LEVEL_TWO_AMOUNT ){
+            return 2;
+        }
+
+        return 1;
+    }
     
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
-        return baseURI();
+        if(getAnimationLevel(tokenId) == 3){
+            return "ipfs://QmdRmgg5SdpAVKpovwrQifSGvN1Y9PS5hQ2nDNWP5dL2uC";
+        }
+
+        if(getAnimationLevel(tokenId) == 2){
+            return "ipfs://QmUTnSkfTgxfaJD5qvfpb4Ltzks5ZLUqvQAAWmTZYRQ8vZ";
+        }
+
+        return "ipfs://QmTTePEgy2g8Yrz1Zq4wB8RGQu9RM5iwB4Ux1U5GP3HxgA";
     }
 
 }
--- deploy/airdrop.ts
+++ deploy/airdrop.ts
@@ -18,7 +18,7 @@ const deployOptions: DeployFunction = async (hre) => {
   const tellerOptions = await deploy({
     contract: 'AirdropToken',
     name: 'AirdropToken',
-    args: ['Airdrop Token','ADT','0xc2ea70ed0d861a2536d5a15b3c4acbb72dcd577b4d75d8c07743c716d813bf4d'],
+    args: ['Airdrop Token','ADT','0xc2ea70ed0d861a2536d5a15b3c4acbb72dcd577b4d75d8c07743c716d813bf4d','0xb6ed7644c69416d67b522e20bc294a9a9b405b31'],
     hre
     
   }) 
